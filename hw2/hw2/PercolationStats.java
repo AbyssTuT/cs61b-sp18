79c58938325a590ad46c5539fa45b071465c8c58
AbyssTuT <73459040+AbyssTuT@users.noreply.github.com>
2022-06-13 16:54:02 +0800

    complete a few bugs

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,25 +4,22 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private int N;
     private int T;
     private double[] fraction;
 
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
-        fraction = new double[T];
-
         if (N <= 0 || T <= 0)
             throw new IllegalArgumentException("N,T must > 0");
+        this.T = T;
+        fraction = new double[T];
 
         for (int i = 0; i < T; i++) {
             Percolation perc = pf.make(N);
             while (!perc.percolates()) {
-                int row = StdRandom.uniform(0, N - 1);
-                int col = StdRandom.uniform(0, N - 1);
-                if (!perc.isOpen(row, col)) {
-                    perc.open(row, col);
-                }
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                perc.open(row, col);
             }
             fraction[i] = (double) perc.numberOfOpenSites() / N / N;
         }
